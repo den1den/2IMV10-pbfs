96cba5fdea34ba8e0f246524a906b01cda3cc5c3
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2017-02-22 13:33:38 +0100

    Simple particle model

--- Assets/Scripts/Unity/ParticleModel.cs
+++ Assets/Scripts/Unity/ParticleModel.cs
@@ -8,12 +8,42 @@ using UnityEngine;
 /// </summary>
 public class ParticleModel : MonoBehaviour
 {
+    Vector3[] positions;
+    Vector3[] velocities;
+    float[] masses;
+
     ParticleModelCalculator pmc;
 
     // Use this for initialization
     void Start()
     {
         pmc = new ParticleModelCalculator(this);
+        const int RES = 50;
+
+        const float dx = 200f / RES; // total size devided by resolution 
+        const float dy = dx;
+        const float dz = dx;
+
+        positions = new Vector3[RES * RES * RES];
+
+        for (int x = 0; x < RES; x++)
+        {
+            for(int y = 0; y < RES; y++)
+            {
+                for (int z = 0; z < RES; z++)
+                {
+                    positions[x * RES * RES + y * RES + z] = new Vector3(x * dx, y * dy, z * dz);
+                }
+            }
+        }
+
+        masses = new float[positions.Length];
+        for(int i = 0; i < masses.Length; i++)
+        {
+            masses[i] = 1f;
+        }
+
+        velocities = new Vector3[positions.Length];
     }
 
     // Update is called once per frame
