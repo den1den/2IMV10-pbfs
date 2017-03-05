160d98f3025ed9ab3823989716ae4bcf526ad565
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2017-03-05 16:25:57 +0100

    Changed to 2D model of a cloth

--- Assets/Scripts/Simulation/EnergyFunctions/FEMFunction.cs
+++ Assets/Scripts/Simulation/EnergyFunctions/FEMFunction.cs
@@ -6,59 +6,68 @@ using UnityEngine;
 
 namespace Assets.Scripts.Simulation.EnergyFunctions
 {
+    /// <summary>
+    /// FEM Function for a triangle
+    /// </summary>
     public class FEMFunction : EnergyFunction
     {
         const float EPS = 1e-6f;
 
         ParticleModel pm;
-        int i0, i1, i2, i3;
+        int i0, i1, i2;
         float volume;
         Matrix4x4 invRestMat;
-        public FEMFunction(ParticleModel pm, int i0, int i1, int i2, int i3, float volume, Matrix4x4 invRestMat)
+        private FEMFunction(ParticleModel pm, int i0, int i1, int i2, float volume, Matrix4x4 invRestMat)
         {
             this.pm = pm;
             this.i0 = i0;
             this.i1 = i1;
             this.i2 = i2;
-            this.i3 = i3;
             this.volume = volume;
             this.invRestMat = invRestMat;
         }
         public Vector3 d0, d1, d2, d3;
 
-        /// <summary>Create FEMFunction (finite element method)</summary>
+        /// <summary>Create FEMFunction for a triangle (finite element method)</summary>
         /// <param name="pm">The actual ParticleModel</param>
-        /// <param name="i0">Index of 0 particle of tetrahedron</param>
-        /// <param name="i1">Index of 1 particle of tetrahedron</param>
-        /// <param name="i2">Index of 2 particle of tetrahedron</param>
-        /// <param name="i3">Index of 3 particle of tetrahedron</param>
-        public static FEMFunction create(ParticleModel pm, int i0, int i1, int i2, int i3)
+        /// <param name="i0">Index of 0 particle of triangle</param>
+        /// <param name="i1">Index of 1 particle of triangle</param>
+        /// <param name="i2">Index of 2 particle of triangle</param>
+        public static FEMFunction create(ParticleModel pm, int i0, int i1, int i2)
         {
             Vector3 p0 = pm.positions[i0];
             Vector3 p1 = pm.positions[i1];
             Vector3 p2 = pm.positions[i2];
-            Vector3 p3 = pm.positions[i3];
 
-            Matrix4x4 m = Util.createM(p0 - p3, p1 - p3, p2 - p3);
+            Vector3 normal = Vector3.Cross(p1 - p0, p2 - p0);
+            Vector3 axis01 = (p2 - p0).normalized;
+            Vector3 axis02 = Vector3.Cross(normal, axis01).normalized;
+
+            // Transform vectors to new cooridinate system
+            Vector2 t0 = new Vector2(Vector3.Dot(p0, axis01), Vector3.Dot(p0, axis02));
+            Vector2 t1 = new Vector2(Vector3.Dot(p1, axis01), Vector3.Dot(p1, axis02));
+            Vector2 t2 = new Vector2(Vector3.Dot(p2, axis01), Vector3.Dot(p2, axis02));
+
+            Matrix4x4 m = new Matrix4x4();
+            m[0, 0] = t0.y - t2.y;
+            m[1, 0] = t0.x - t2.x;
+            m[0, 1] = t1.y - t2.y;
+            m[1, 1] = t1.x - t2.x;
+            m[2, 2] = 1;
+            m[3, 3] = 1;
             float det = m.determinant;
 
             if(Math.Abs(det) > EPS)
             {
                 Matrix4x4 invRestMat = m.inverse;
-
-                float volume = calcVolume(p0, p1, p2, p3);
-
-                return new FEMFunction(pm, i0, i1, i2, i3, volume, invRestMat);
+                // Calculate area
+                float area = Math.Abs(normal.magnitude) * 0.5f;
+                Debug.Log("Triangle (" + p0 + ", " + p1 + ", " + p2 + ")");
+                Debug.Log("Has area " + area + "\n");
+                return new FEMFunction(pm, i0, i1, i2, area, invRestMat);
             }
+            // determinant ~= 0
             return null;
-        }
-
-        private static float calcVolume(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
-        {
-            float volume = Math.Abs(1f / 6 * Vector3.Dot(p3 - p0, Vector3.Cross(p2 - p0, p1 - p0)));
-            Debug.Log("Thetrahedon (" + p0 + ", " + p1 + ", " + p2 + ", " + p3 + ")");
-            Debug.Log("Has volume " + volume + "\n");
-            return volume;
         }
 
         public void solve()
@@ -67,7 +76,8 @@ namespace Assets.Scripts.Simulation.EnergyFunctions
             d1 = Vector3.zero;
             d2 = Vector3.zero;
             d3 = Vector3.zero;
-            // TODO
+            // TODO: https://github.com/InteractiveComputerGraphics/PositionBasedDynamics/blob/master/PositionBasedDynamics/PositionBasedDynamics.cpp#L882
         }
     }
 }
+
--- Assets/Scripts/Unity/ParticleModel.cs
+++ Assets/Scripts/Unity/ParticleModel.cs
@@ -16,7 +16,7 @@ public class ParticleModel : MonoBehaviour
     public Vector3[] velocities;
     public float[] masses;
 
-    public int[] tetras;
+    public int[] triangleIndices;
 
     public EnergyFunction[] efs;
 
@@ -24,73 +24,66 @@ public class ParticleModel : MonoBehaviour
 
     ParticleVisualisation simpleVis;
 
-    void initRect(float dx, float dy, float dz, int nx, int ny, int nz)
+    /// <summary>
+    /// Set this.positions 
+    /// </summary>
+    void initPoints(float dx, float dy, int nx, int ny, float zCoord)
     {
-        // Positions
-        positions = new Vector3[nx * ny * nz];
+        // set positions
+        positions = new Vector3[nx * ny];
         for (int x = 0; x < nx; x++)
             for (int y = 0; y < ny; y++)
-                for (int z = 0; z < nz; z++)
-                {
-                    positions[getRectI(nz, ny, x, y, z)] = new Vector3(x * dx, y * dy, z * dz);
-                }
+                    positions[x * ny + y] = new Vector3(x * dx, y * dy, zCoord); // row major
 
-        // Tetrahedrons
-        tetras = new int[nx * ny * nz * 20];
+        triangleIndices = new int[(nx - 1) * (ny - 1) * 3 * 2];
+        int i = 0;
         for (int x0 = 0; x0 < nx - 1; x0++)
         {
             int x1 = x0 + 1;
             for (int y0 = 0; y0 < ny - 1; y0++)
             {
                 int y1 = y0 + 1;
-                for (int z0 = 0; z0 < nz - 1; z0++)
+                // Create 2 triangles for each square
+                if ((x0 + y0) % 2 == 0) // Alternate the ordering of the triangles
                 {
-                    int z1 = z0 + 1;
-                    int p0 = getRectI(ny, nz, x0, y0, z0);
-                    int p1 = getRectI(ny, nz, x0, y0, z1);
-                    int p2 = getRectI(ny, nz, x1, y0, z1);
-                    int p3 = getRectI(ny, nz, x1, y0, z0);
-                    int p4 = getRectI(ny, nz, x0, y1, z0);
-                    int p5 = getRectI(ny, nz, x0, y1, z1);
-                    int p6 = getRectI(ny, nz, x1, y1, z1);
-                    int p7 = getRectI(ny, nz, x1, y1, z0);
-                    // Alternate the order
-                    if ((x0 + y0 + z0) % 2 == 1)
-                    {
-                        Array.Copy(new int[] {
-                            p2, p1, p6, p3,
-                            p6, p3, p4, p7,
-                            p4, p1, p6, p5,
-                            p3, p1, p4, p0,
-                            p6, p1, p4, p3
-                        }, 0, tetras, p0 * 20, 20);
-                    } else
-                    {
-                        Array.Copy(new int[] {
-                            p0, p2, p5, p1,
-                            p7, p2, p0, p3,
-                            p5, p2, p7, p6,
-                            p7, p0, p5, p4,
-                            p0, p2, p7, p5
-                        }, 0, tetras, p0 * 20, 20);
-                    }
+                    // 10 - 11
+                    // |  \ |
+                    // 00 - 01
+                    // the two triangles are (00, 01, 10) and (11, 10, 01)
+                    triangleIndices[i++] = x0 * ny + y0;
+                    triangleIndices[i++] = x0 * ny + y1;
+                    triangleIndices[i++] = x1 * ny + y0;
+                    triangleIndices[i++] = x1 * ny + y1;
+                    triangleIndices[i++] = x1 * ny + y0;
+                    triangleIndices[i++] = x0 * ny + y1;
+                }
+                else
+                {
+                    // 10 - 11
+                    // |  / |
+                    // 00 - 01
+                    // the two triangles are (00, 01, 11) and (11, 10, 00)
+                    triangleIndices[i++] = x0 * ny + y0;
+                    triangleIndices[i++] = x0 * ny + y1;
+                    triangleIndices[i++] = x1 * ny + y1;
+                    triangleIndices[i++] = x1 * ny + y1;
+                    triangleIndices[i++] = x1 * ny + y0;
+                    triangleIndices[i++] = x0 * ny + y0;
                 }
             }
         }
 
-        // Masses
+        // Set masses
         masses = new float[positions.Length];
-        for (int i = 0; i < masses.Length; i++)
+        for (i = 0; i < masses.Length; i++)
         {
             masses[i] = 1f;
         }
-        // Everywhere 1, except for x = 0
-        for (int y = 0; y < ny; y++)
-            for (int z = 0; z < nz; z++)
-            {
-                //masses[getRectI(ny, nz, 0, y, z)] = 0f;
-                // Make sure this goes well in first ParticleModelCalculation step
-            }
+
+        // Set masses of endpoints at y=0 to zero to make it static
+        // TODO: mass of 0 = static point
+        // masses[0] = 0; // x=0, y=0
+        // masses[(nx - 1) * ny + 0] = 0; // x = nx, y = 0
     }
 
     // Use this for initialization
@@ -100,7 +93,8 @@ public class ParticleModel : MonoBehaviour
         const int RES = 7;
         const float SIZE = 200f;
         const float D = SIZE / RES; // dx, dy, dz = total size devided by resolution 
-        initRect(D, D, D, RES, RES, RES);
+        const float zCoord = 1;
+        initPoints(D, D, RES, RES, zCoord);
         Debug.Log(positions.Length + " particles created");
 
         // Create constraints on particles
@@ -116,28 +110,29 @@ public class ParticleModel : MonoBehaviour
 
     private void initConstraints()
     {
-        // loop over every tetrahedron
-        int nTets = tetras.Length / 4;
+        // https://github.com/InteractiveComputerGraphics/PositionBasedDynamics/blob/master/Demos/ClothDemo/main.cpp#L354
+        // loop over every triangle
+
+        int nTriangles = triangleIndices.Length / 3;
         List<EnergyFunction> efs = new List<EnergyFunction>();
 
-        for (int i = 0; i < nTets; i++)
+        for (int i = 0; i < nTriangles; i++)
         {
-            int p0 = tetras[i + 0];
-            int p1 = tetras[i + 1];
-            int p2 = tetras[i + 2];
-            int p3 = tetras[i + 3];
+            int i0 = triangleIndices[i + 0];
+            int i1 = triangleIndices[i + 1];
+            int i2 = triangleIndices[i + 2];
 
             // Only add FEMTetConstriant for now
-            EnergyFunction fem = FEMFunction.create(this, p0, p1, p2, p3);
+            EnergyFunction fem = FEMFunction.create(this, i0, i1, i2);
             if(fem != null)
             {
                 efs.Add(fem);
             }
 
             // TODO: OR we can only add Distance and Volume constraints
-            // https://github.com/InteractiveComputerGraphics/PositionBasedDynamics/blob/36f571d27718f8d3fca9cd3344d47f2c3a1e4a09/Demos/BarDemo/main.cpp#L326
+            // https://github.com/InteractiveComputerGraphics/PositionBasedDynamics/blob/master/Demos/ClothDemo/main.cpp#L340
         }
-        Debug.Log(efs.Count + " FEMFunctions created for " + nTets + " thetrahedons");
+        Debug.Log(efs.Count + " FEMFunctions created for " + nTriangles + " triangles");
         this.efs = efs.ToArray();
     }
 
@@ -148,10 +143,5 @@ public class ParticleModel : MonoBehaviour
         pmc.Update(Time.deltaTime);
         // Show new positions
         simpleVis.UpdatePositions(positions);
-    }
-
-    public static int getRectI(int ny, int nz, int x, int y, int z)
-    {
-        return x * ny * nz + y * nz + z;
     }
 }
